2ee0ce79b685715d4db8baf186d9b45ce4f97e7f
L1ghtDream <radu.voinea.mihai@gmail.com>
2022-04-04 22:57:56 +0300

    Added version in activity

--- src/main/java/dev/lightdream/jdaextension/JDAExtensionMain.java
+++ src/main/java/dev/lightdream/jdaextension/JDAExtensionMain.java
@@ -1,13 +1,14 @@
 package dev.lightdream.jdaextension;
 
 import dev.lightdream.jdaextension.dto.JDAConfig;
-import dev.lightdream.jdaextension.dto.JDALang;
 import dev.lightdream.jdaextension.dto.JDAEmbed;
+import dev.lightdream.jdaextension.dto.JDALang;
 import dev.lightdream.jdaextension.managers.DiscordCommandManager;
 import dev.lightdream.lambda.LambdaExecutor;
 import dev.lightdream.logger.Logger;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import javax.security.auth.login.LoginException;
@@ -17,15 +18,16 @@ import java.util.List;
 public interface JDAExtensionMain {
 
     @SuppressWarnings("unused")
-    static JDA generateBot(String token) {
-        return generateBot(token, new ArrayList<>());
+    static JDA generateBot(JDAExtensionMain main, String token) {
+        return generateBot(main, token, new ArrayList<>());
     }
 
-    static JDA generateBot(String token, List<GatewayIntent> gatewayIntents) {
+    static JDA generateBot(JDAExtensionMain main, String token, List<GatewayIntent> gatewayIntents) {
         return LambdaExecutor.LambdaCatch.ReturnLambdaCatch.executeCatch(() -> {
             try {
                 return JDABuilder.createDefault(token)
                         .enableIntents(gatewayIntents)
+                        .setActivity(Activity.playing("v" + main.getVersion() + " LightDream#4379"))
                         .build();
             } catch (LoginException e) {
                 Logger.error("The bot token seems to be missing or incorrect, please check if it!");
@@ -33,6 +35,8 @@ public interface JDAExtensionMain {
             }
         });
     }
+
+    String getVersion();
 
     JDA getBot();
 
